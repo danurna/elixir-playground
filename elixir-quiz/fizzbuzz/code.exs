60af96a58bfc61c09878aa02c2d550361a10943c
Daniel Witurna <daniel.witurna@gmail.com>
2019-08-31 12:36:23 +0100

    Add more concise version by creating a list

--- elixir-quiz/fizzbuzz/code.exs
+++ elixir-quiz/fizzbuzz/code.exs
@@ -1,21 +1,5 @@
 defmodule FizzBuzz do 
-  
-  def execute(upperRange) when is_number(upperRange) do
-    execute(1, upperRange)
-  end 
-
-  defp execute(l, u) when l < u do 
-    fizzbuzz(l)
-      |> IO.puts
-    execute(l + 1, u)
-  end
-
-  defp execute(l, _u) do 
-    fizzbuzz(l)
-      |> IO.puts
-  end
-
-  defp fizzbuzz(number) do
+  def convert(number) do
     cond do 
       rem(number, 3) == 0 and rem(number, 5) == 0 -> "FizzBuzz"
       rem(number, 3) == 0 -> "Fizz"
@@ -26,9 +10,36 @@ defmodule FizzBuzz do
 
 end
 
+defmodule PrintFizzBuzz do
+  def execute(upperRange) when is_number(upperRange) do
+    execute(1, upperRange)
+  end 
+
+  defp execute(l, u) when l < u do 
+    FizzBuzz.convert(l)
+      |> IO.puts
+    execute(l + 1, u)
+  end
+
+  defp execute(l, _u) do 
+    FizzBuzz.convert(l)
+      |> IO.puts
+  end
+end
+
+defmodule ListFizzBuzz do 
+  def execute(max) when is_number(max) do 
+    Enum.reduce(1..max, [], &([ FizzBuzz.convert(&1) | &2 ]))
+      |> Enum.reverse
+      |> Enum.join(" ")
+  end
+end
+
 defmodule Main do 
   def main() do 
-    FizzBuzz.execute(100)
+    PrintFizzBuzz.execute(100)
+    ListFizzBuzz.execute(100)
+      |> IO.puts 
   end
 end
 
