c711ec623f82728ad829f60c457e53f5a9a1f0bf
Daniel Witurna <daniel.witurna@gmail.com>
2019-08-31 12:38:00 +0100

    Delete naive IO print fizz buzz

--- elixir-quiz/fizzbuzz/code.exs
+++ elixir-quiz/fizzbuzz/code.exs
@@ -1,3 +1,11 @@
+defmodule ListFizzBuzz do 
+  def execute(max) when is_number(max) do 
+    Enum.reduce(1..max, [], &([ FizzBuzz.convert(&1) | &2 ]))
+      |> Enum.reverse
+      |> Enum.join(" ")
+  end
+end
+
 defmodule FizzBuzz do 
   def convert(number) do
     cond do 
@@ -7,37 +15,10 @@ defmodule FizzBuzz do
       true -> number
     end
   end
-
-end
-
-defmodule PrintFizzBuzz do
-  def execute(upperRange) when is_number(upperRange) do
-    execute(1, upperRange)
-  end 
-
-  defp execute(l, u) when l < u do 
-    FizzBuzz.convert(l)
-      |> IO.puts
-    execute(l + 1, u)
-  end
-
-  defp execute(l, _u) do 
-    FizzBuzz.convert(l)
-      |> IO.puts
-  end
-end
-
-defmodule ListFizzBuzz do 
-  def execute(max) when is_number(max) do 
-    Enum.reduce(1..max, [], &([ FizzBuzz.convert(&1) | &2 ]))
-      |> Enum.reverse
-      |> Enum.join(" ")
-  end
 end
 
 defmodule Main do 
   def main() do 
-    PrintFizzBuzz.execute(100)
     ListFizzBuzz.execute(100)
       |> IO.puts 
   end
