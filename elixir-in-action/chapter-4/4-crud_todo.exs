5decb05023f0100bc4bacdf54fb2803296738645
Daniel Witurna <daniel.witurna@gmail.com>
2019-09-02 22:18:55 +0100

    Add update entries function

--- elixir-in-action/chapter-4/4-crud_todo.exs
+++ elixir-in-action/chapter-4/4-crud_todo.exs
@@ -23,7 +23,16 @@ defmodule TodoList do
       |> Enum.map(fn {_, entry} -> entry end)
   end
 
-  #def update_entry(todo_list, 
+  def update_entry(todo_list, entry_id, updater_fun) do 
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error -> 
+        todo_list
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry) 
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end 
 end
 
 ExUnit.start
@@ -46,6 +55,13 @@ defmodule TodoListTests do
       %{date: ~D[2018-01-01], id: 1, title: "Say hi"},
       %{date: ~D[2018-01-01], id: 3, title: "Don't say hi"}
     ]
+  end 
+  
+  test "Item is updated" do
+    todo_list = TodoList.new() 
+        |> TodoList.add_entry(%{date: ~D[2018-01-01], title: "Say hi"})
+        |> TodoList.update_entry(1, &Map.put(&1, :title, "Say bye"))
+    assert TodoList.entries(todo_list, ~D[2018-01-01]) == [%{date: ~D[2018-01-01], id: 1, title: "Say bye"}]
   end 
 end
 
