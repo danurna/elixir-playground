8c5fc3e0a7d4baab5a6d4f0f1f052914dd0280f2
Daniel Witurna <daniel.witurna@gmail.com>
2019-09-21 10:52:00 +0100

    Add cache tests

--- elixir-in-action/ch-7/todo_cache/test/todo_server_test.exs
+++ elixir-in-action/ch-7/todo_cache/test/todo_server_test.exs
@@ -4,8 +4,8 @@ defmodule TodoServerTests do
   test "Write and read from server" do
     {:ok, server_id} = Todo.Server.start()
     Todo.Server.add_entry(server_id, %{date: ~D[2018-01-01], title: "Alpha"})
-    assert Todo.Server.entries(server_id, ~D[2018-01-01]) == [ 
-     %{date: ~D[2018-01-01], id: 1, title: "Alpha"}
-    ]
+    entries = Todo.Server.entries(server_id, ~D[2018-01-01]) 
+
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Alpha"}] = entries
   end
 end
