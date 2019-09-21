9580ceb1f72bb14e3991bc64415219096fce02f9
Daniel Witurna <daniel.witurna@gmail.com>
2019-09-21 20:19:18 +0100

    Introduce pooling by using database workers

--- elixir-in-action/ch-7/persistable_todo_cache/lib/todo/database.ex
+++ elixir-in-action/ch-7/persistable_todo_cache/lib/todo/database.ex
@@ -2,6 +2,7 @@ defmodule Todo.Database do
   use GenServer
 
   @db_folder "./persist"
+  @pool_size 3
 
   def start do 
     GenServer.start(__MODULE__, nil,
@@ -20,22 +21,78 @@ defmodule Todo.Database do
   @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
-    {:ok, nil}
+
+    workers = Enum.reduce(
+      0..@pool_size-1,
+      %{},
+      fn index, acc ->
+        {:ok, worker_pid} = Todo.DatabaseWorker.start(@db_folder) 
+        Map.put(acc, index, worker_pid)
+      end
+    )
+    IO.inspect(workers)
+    {:ok, workers}
   end
 
   @impl GenServer
-  def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
-    
-    {:noreply, state} 
+  def handle_cast({:store, key, data}, workers) do
+    worker = choose_worker(workers, key)
+    GenServer.cast(worker, {:store, key, data}) 
+  
+    {:noreply, workers} 
   end
   
   @impl GenServer
-  def handle_call({:get, key}, _, state) do
+  def handle_call({:get, key}, _, workers) do
+    worker = choose_worker(workers, key)
+    data = GenServer.call(worker, {:get, key})
+    {:reply, data, workers} 
+  end
+
+  defp choose_worker(workers, key) do
+    worker_index = :erlang.phash2(key, @pool_size) 
+    IO.inspect("Choosing #{worker_index} for #{key}")
+    Map.get(workers, worker_index) 
+  end
+end
+
+defmodule Todo.DatabaseWorker do 
+  use GenServer
+
+  def start(db_folder) do 
+    GenServer.start(__MODULE__, db_folder)
+  end
+
+  def store(worker, key, data) do 
+    GenServer.cast(worker, {:store, key, data})
+  end
+
+  def get(worker, key) do 
+    GenServer.call(worker, {:get, key})
+  end
+
+  @impl GenServer
+  def init(db_folder) do
+    {:ok, db_folder}
+  end
+
+  @impl GenServer
+  def handle_cast({:store, key, data}, db_folder) do
+    IO.inspect(self())
+    IO.inspect("Storing #{key}")
+    key
+    |> file_name(db_folder)
+    |> File.write!(:erlang.term_to_binary(data))
+    
+    {:noreply, db_folder} 
+  end
+  
+  @impl GenServer
+  def handle_call({:get, key}, _, db_folder) do
+    IO.inspect(self())
+    IO.inspect("Reading #{key}")
     raw_data = key 
-      |> file_name()
+      |> file_name(db_folder)
       |> File.read()
 
     data = case raw_data do
@@ -43,11 +100,11 @@ defmodule Todo.Database do
       _ -> nil
     end
 
-    {:reply, data, state} 
+    {:reply, data, db_folder} 
   end
 
-  defp file_name(key) do
-    Path.join(@db_folder, to_string(key))
+  defp file_name(key, db_folder) do
+    Path.join(db_folder, to_string(key))
   end
 end
 
