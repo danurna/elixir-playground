a3784f55f7b705006934e85b0e8ca30672b00d04
Daniel Witurna <daniel.witurna@gmail.com>
2019-09-25 09:01:22 +0100

    Add expiry to GenServer to-do server implementation

--- elixir-in-action/ch-9/expiry_todo/lib/todo/server.ex
+++ elixir-in-action/ch-9/expiry_todo/lib/todo/server.ex
@@ -1,26 +1,38 @@
 defmodule Todo.Server do 
   use GenServer, restart: :temporary
 
+  @expiry_idle_timeout :timer.seconds(10)
+
   @impl GenServer
   def init(name) do
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
+    IO.puts("Starting server for #{name}")
+    {
+      :ok, 
+      {name, Todo.Database.get(name) || Todo.List.new()},
+      @expiry_idle_timeout
+    }
   end
 
   @impl GenServer
   def handle_call({:entries, date}, _, {name, list}) do
-    {:reply, Todo.List.entries(list, date), {name, list}}
+    {:reply, Todo.List.entries(list, date), {name, list}, @expiry_idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:add_entry, entry}, {name, list}) do
     updated_list = Todo.List.add_entry(list, entry)  
     Todo.Database.store(name, list) 
-    {:noreply, {name, updated_list}}
+    {:noreply, {name, updated_list}, @expiry_idle_timeout}
+  end
+    
+  @impl GenServer
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping to-do server for #{name}")
+    {:stop, :normal, {name, todo_list}}
   end
 
   # --- Interaction
   def start_link(list_name) do
-    IO.puts("Starting server for #{list_name}")
     GenServer.start_link(__MODULE__, list_name, name: via_tuple(list_name)) 
   end
   
