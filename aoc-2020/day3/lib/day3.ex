2728000b41fc5777084ca570958e5b705adbf097
Daniel Witurna <daniel.witurna@gmail.com>
2020-12-03 19:23:06 +0100

    Add day3 solutions

--- aoc-2020/day3/lib/day3.ex
+++ aoc-2020/day3/lib/day3.ex
@@ -26,21 +26,6 @@ defmodule Part1 do
     |> compute
   end
 
-  # Current line = 0
-  # Current offset = 0
-  # Step = %{right: 3, down: 1}
-  #
-  # [".", ".", "#", "."]
-  # [".", ".", "#", "."]
-  # ["#", ".", ".", "#"]
-
-  # Example --> 4 tree
-  # [".", ".", "# H", "."]
-  # [".", ".", "# H", "."]
-  # ["# H", ".", ".", "# H"]
-
-  # offset = (offset + 2) modulo Grid_width
-
   def compute(lines) do
     _ = length(lines)
     line_width = hd(lines) |> String.length()
@@ -48,38 +33,84 @@ defmodule Part1 do
     step = %{right: 3, down: 1}
     start_pointer = %{x: 0, y: 0}
 
-    Enum.reduce(lines, %{pointer: start_pointer, count: 0}, fn line, state ->
-      start_x = state.pointer.x
-      end_x = state.pointer.x + step.right
+    index_lines = Enum.with_index(lines)
+    Enum.reduce(index_lines, %{pointer: start_pointer, count: 0}, fn {line, idx}, state ->
+      norm_x = rem(state.pointer.x, line_width)
+      updated_count =
+        case String.at(line, norm_x) do
+          "#" ->  state.count + 1
+          _ -> state.count
+        end
 
-      IO.inspect(line)
+      updated_pointer = %{x: state.pointer.x + step.right, y: state.pointer.y + 1}
 
-      updated_state =
-        Enum.reduce([start_x, end_x], state, fn x, h_state ->
-          updated_pointer = %{x: x, y: h_state.pointer.y}
-          norm_x = rem(x, line_width)
-
-          case String.at(line, norm_x) do
-            "#" -> %{pointer: updated_pointer, count: h_state.count + 1} |> IO.inspect
-            _ -> %{pointer: updated_pointer, count: h_state.count}
-          end
-        end)
       %{
-        pointer: %{x: updated_state.pointer.x, y: updated_state.pointer.y + 1},
-        count: updated_state.count
+        pointer: updated_pointer,
+        count: updated_count
       }
     end)
-
   end
 end
 
 defmodule Part2 do
   def run() do
     AOCHelper.read_input()
-    |> compute()
+    |> compute([
+      %{right: 1, down: 1},
+      %{right: 3, down: 1},
+      %{right: 5, down: 1},
+      %{right: 7, down: 1},
+      %{right: 1, down: 2}
+      ])
   end
 
-  def compute(_lines) do
+  def debug_sample() do
+    [
+      "..##.......",
+      "#...#...#..",
+      ".#....#..#.",
+      "..#.#...#.#",
+      ".#...##..#.",
+      "..#.##.....",
+      ".#.#.#....#",
+      ".#........#",
+      "#.##...#...",
+      "#...##....#",
+      ".#..#...#.#",
+    ]
+    |> compute([
+      %{right: 3, down: 1}
+      ])
+  end
+
+  def compute(lines, slopes) do
+    _ = length(lines)
+    line_width = hd(lines) |> String.length()
+
+    Enum.reduce(slopes, %{sum: 1}, fn slope, state ->
+
+      filtered_lines = Enum.take_every(lines, slope.down)
+      slope_result =
+        Enum.reduce(filtered_lines, %{pointer: %{x: 0, y: 0}, count: 0}, fn line, state ->
+          norm_x = rem(state.pointer.x, line_width)
+          updated_count =
+            case String.at(line, norm_x) do
+              "#" ->  state.count + 1
+              _ -> state.count
+            end
+
+          updated_pointer = %{x: state.pointer.x + slope.right, y: state.pointer.y + slope.down}
+
+          %{
+            pointer: updated_pointer,
+            count: updated_count
+          }
+        end)
+
+      %{
+        sum: state.sum * slope_result.count
+      }
+    end)
   end
 end
 
