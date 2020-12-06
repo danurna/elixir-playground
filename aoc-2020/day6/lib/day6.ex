fb66196d025a8d07b385e772f92622b44882b832
Daniel Witurna <daniel.witurna@gmail.com>
2020-12-06 12:25:44 +0100

    Add Day6 Part 2 solution

--- aoc-2020/day6/lib/day6.ex
+++ aoc-2020/day6/lib/day6.ex
@@ -6,7 +6,7 @@ defmodule Day6 do
 
   def run_part2() do
     AOCHelper.read_input()
-    |> AnswerValidator.count_any_positive()
+    |> AnswerValidator.count_common_positives()
   end
 
   def debug_sample() do
@@ -27,11 +27,13 @@ defmodule Day6 do
       "",
       "b"
     ]
-    |> AnswerValidator.count_any_positive()
+    |> AnswerValidator.count_common_positives()
   end
 end
 
 defmodule AnswerValidator do
+  # Collect answers per group by putting it into one string
+  # and count the unique characters afterwards
   def count_any_positive(lines) do
     lines
     |> Enum.reduce([""], fn line, acc ->
@@ -44,6 +46,31 @@ defmodule AnswerValidator do
     end)
     |> Enum.map(&String.graphemes/1)
     |> Enum.map(&Enum.uniq/1)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.sum
+  end
+
+  # Collect answers per groups and per person
+  # and intersect them inside groups
+  def count_common_positives(lines) do
+    lines
+    |> Enum.reduce([[]], fn line, acc ->
+      case line do
+        "" -> [[] | acc]
+        line ->
+          [group | other_groups] = acc
+          new_set =
+            line
+            |> String.graphemes
+            |> Enum.reduce(MapSet.new, fn c, s -> MapSet.put(s, c) end)
+          [[new_set | group] | other_groups]
+      end
+    end)
+    |> Enum.map(fn group ->
+      Enum.reduce(group, hd(group), fn item, combined ->
+        MapSet.intersection(combined, item)
+      end)
+    end)
     |> Enum.map(&Enum.count/1)
     |> Enum.sum
   end
