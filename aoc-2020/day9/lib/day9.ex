73270e486c3a3f23b300c9075a9a5e7510d71677
Daniel Witurna <daniel.witurna@gmail.com>
2020-12-09 23:02:45 +0100

    Add Day 9 Part 2 Solution

--- aoc-2020/day9/lib/day9.ex
+++ aoc-2020/day9/lib/day9.ex
@@ -6,9 +6,15 @@ defmodule Day9 do
   end
 
   def run_part2() do
-    AOCHelper.read_input()
-    |> Enum.map(&String.to_integer/1)
-    |> Attacker.first_invalid_number(25)
+    input =
+      AOCHelper.read_input()
+      |> Enum.map(&String.to_integer/1)
+
+    invalid_number = Attacker.first_invalid_number(input, 25)
+    Attacker.find_set(input, invalid_number)
+    |> Enum.min_max()
+    |> Tuple.to_list()
+    |> Enum.sum()
   end
 
   def debug_sample() do
@@ -35,11 +41,29 @@ defmodule Day9 do
       "576"
     ]
     |> Enum.map(&String.to_integer/1)
-    |> Attacker.first_invalid_number(5)
+    |> Attacker.find_set(127)
   end
 end
 
 defmodule Attacker do
+  # Part 2
+  def find_set([], number), do: {:error, :no_solution}
+  def find_set(input, number) do
+    case sum_with_next(input, number, []) do
+      {:error, _} -> find_set(tl(input), number)
+      {:ok, set} -> set
+    end
+  end
+
+  defp sum_with_next([head | tail], number, set) do
+    updated_set = [head | set]
+    case Enum.sum(updated_set) do
+      ^number -> {:ok, updated_set}
+      x when x > number -> {:error, :exceeded}
+      _ -> sum_with_next(tail, number, updated_set)
+    end
+  end
+
   # Part 1
   def first_invalid_number([], _preamble_size) do
     {:ok, :all_valid}
